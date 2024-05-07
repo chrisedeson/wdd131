57fdcab8d604d8f16277503e70a9fe1c9cbb2b5b
chrisedeson <eff24002@byui.edu>
2024-05-07 21:32:33 +0100

    style the footer

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,17 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.getElementById("menu-btn");
-  const menuIcon = document.getElementById("menu-icon");
-  // bi-x-lg
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
-  menuBtn.addEventListener("click", function () {
-    const nav = document.getElementById("nav");
-    if (nav.style.display === "flex") {
-      nav.style.display = "none";
-      menuIcon.classList.replace("bi-x-lg", "bi-list");
-    } else {
-      nav.style.display = "flex";
-      menuIcon.classList.replace("bi-list", "bi-x-lg");
-    }
 
-    // const nav = document.querySelector("#nav") mapear css selector
-  });
+// hamButton.addEventListener('click', () => {
+// 	navigation.classList.toggle('open');
+// 	hamButton.classList.toggle('open');
+// });
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+
+// Get the current year for copyright
+const currentYear = new Date().getFullYear();
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+
+// Update the footer's first paragraph with the copyright year
+document.getElementById("currentyear").textContent = currentYear;
+
+// Update the footer's second paragraph with the last modified date
+const lastModifiedSpan = document.getElementById("lastModified");
+lastModifiedSpan.textContent = `Last Modification: ${lastModifiedDate}`;
