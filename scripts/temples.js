55ebac90275850bd85ed4aceb8cf419cd3dae1f5
chrisedeson <eff24002@byui.edu>
2024-05-13 01:26:24 +0100

    week 2 - temple album

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,29 +1,16 @@
-// Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+  toggleButton.classList.toggle('open');
 
-// hamButton.addEventListener('click', () => {
-// 	navigation.classList.toggle('open');
-// 	hamButton.classList.toggle('open');
+})
+
+// const toggleButton = document.querySelector('.toggle-button');
+// const navbarLinks = document.querySelector('.navbar-links');
+
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active');
+//     toggleButton.classList.toggle('open');
 // });
-
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
-
-
-// Get the current year for copyright
-const currentYear = new Date().getFullYear();
-
-// Get the last modified date of the document
-const lastModifiedDate = document.lastModified;
-
-// Update the footer's first paragraph with the copyright year
-document.getElementById("currentyear").textContent = currentYear;
-
-// Update the footer's second paragraph with the last modified date
-const lastModifiedSpan = document.getElementById("lastModified");
-lastModifiedSpan.textContent = `Last Modification: ${lastModifiedDate}`;
